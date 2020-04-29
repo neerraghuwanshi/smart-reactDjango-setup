c0eed389a4d88765981e1596c137c0520e4e0426
NEER RAGHUWANSHI <neerraghuwanshi@NEERs-MacBook-Pro.local>
2020-04-29 16:04:35 +0530

    improved

--- backend/settings.py
+++ backend/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = config('SecretKey')
+SECRET_KEY = os.getenv('SecretKey')
 
 DEBUG = True
 
--- backend/urls.py
+++ backend/urls.py
@@ -25,3 +25,8 @@ urlpatterns = [
     re_path(r'^frontend*', TemplateView.as_view(template_name='index.html')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
